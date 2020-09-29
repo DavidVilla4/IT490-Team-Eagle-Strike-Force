4401ca22116e42b8b05c7937f52d98d262568232
DavidVilla4 <devprogram4@gmail.com>
2020-09-29 11:44:40 -0400

    first website commit

---
+++ LoginRabbitMQ.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+if(isset($_POST))
+{
+    $type = "Login";
+    $email = $_POST["email"];
+    $password = $_POST["password";
+}
+$request = array();
+$request['type'] = $type;
+$request['email'] = $email;
+$request['password'] = $password;
+$request['message'] = $msg;
+
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+
+
