3793dd10dfff73519213421d3f3a25996b17eea7
kwp5 <kwp5@njit.edu>
2020-10-05 23:45:39 -0400

    Added Logging Services

---
+++ loggingServer.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function requestProcessor($request)
+{
+	var_dump($request);
+	echo "Sending Logs".PHP_EOL;
+	return $request['logs'];
+}
+
+$server = new rabbitMQServer("logging.ini","testServer");
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
