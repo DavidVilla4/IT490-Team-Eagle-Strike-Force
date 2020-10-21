ed5a42fd3a953fef6d0bc73cb157407dda078411
kwp5 <kwp5@njit.edu>
2020-10-21 15:08:08 -0400

    Adjusted sql queries Added Number of Votes to return logs

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,6 +4,116 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+function doVoting($recipeName,$review,$logs)
+{
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    try {
+	$mydb = new mysqli('127.0.0.1','admin','admin','newDb');
+	if ($mydb->errno != 0)
+	{
+		$msg = "Failed to connect to database: ". $mydb->error . PHP_EOL;
+		array_push($logs,$msg);
+		exit($logs);
+	}
+	$msg = "Connected to database".PHP_EOL;
+	array_push($logs,$msg);
+	if ($review == "up")
+       	{
+		$query = "UPDATE field_name SET vote = vote + 1 WHERE recipeName = $recipeName";
+		$result = $mydb->query($query);
+		if ($mydb->errno != 0)
+		{
+			$msg = "Failed to execute query: ".PHP_EOL;
+			$msg1 = __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+			array_push($logs,$msg,$msg1);
+			exit($logs);
+		}
+		if ($result)
+		{
+			$msg = "Recipe ". $recipeName . "was upvoted".PHP_EOL;
+			array_push($logs,$msg);
+		}
+	}
+	else if ($review == "no")
+       	{
+		$query = "UPDATE field_name SET vote = vote - 1 WHERE recipeName = $recipeName";
+		$result = $mydb->query($query);
+		if ($mydb->errno != 0)
+		{
+			$msg = "Failed to execute query: ".PHP_EOL;
+			$msg1 = __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+			array_push($logs,$msg,$msg1);
+			exit($logs);
+		}
+		if ($result)
+		{
+			$msg = "Recipe ". $recipeName . "was downvoted".PHP_EOL;
+			array_push($logs,$msg);
+		}
+	}
+	else
+       	{
+		$msg = "Incorrect value for review".PHP_EOL;
+		array_push($logs,$msg);
+		exit($logs);
+	}
+	$query = "SELECT field_name FROM Recipes WHERE recipeName = '$recipeName'";
+	$result = $mydb->query($query);
+	if ($mydb->errno != 0)
+	{
+		$msg = "Failed to execute query: ".PHP_EOL;
+		$msg1 = __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+		array_push($logs,$msg,$msg1);
+		exit($logs);
+	}
+	if ($result)
+	{
+		$row = mysqli_fetch_array($result);
+		$logs['voteCount'] = $row['field_name'];
+	}
+	return $logs;
+    }
+    catch(mysqli_sql_exception $e) {
+	    array_push($logs,$e->getMessage());
+	    return $logs;
+    }
+}
+
+function doAddRec($recName,$logs)
+{	
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);   
+    try {    
+	$mydb = new mysqli('127.0.0.1','admin','admin','newDb');
+	if ($mydb->errno != 0)
+	{
+		$msg = "Failed to connect to database: ". $mydb->error . PHP_EOL;
+		array_push($logs,$msg);
+		exit($logs);
+	}
+	$msg = "Connected to Database, Checking database for user".PHP_EOL;
+	array_push($logs,$msg);
+	$query = "INSERT INTO Recipes (name) VALUES ('$recipeName')";
+	$result = $mydb->query($query);
+	if ($mydb->errno != 0)
+	{
+		$msg = "Failed to execute query: ".PHP_EOL;
+		$msg1 = __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+		array_push($logs,$msg,$msg1);
+		exit($logs);
+	}
+	if ($result)
+	{
+		$msg = "Recipe Added".PHP_EOL;
+		array_push($logs,$msg);
+		exit($logs);
+	}
+    }
+    catch(mysqli_sql_exception $e) {
+	    array_push($logs,$e->getMessage());
+	    return $logs;
+    }
+}
+
 function doLogin($email,$password,$logs)
 {
     // lookup username in databas
@@ -19,7 +129,7 @@ function doLogin($email,$password,$logs)
 	}
 	$msg = "Connected to Database, Checking database for user".PHP_EOL;
 	array_push($logs,$msg);
-	$query = "SELECT * FROM `USERS` WHERE `email` = '$email' AND `password` = '$password'";
+	$query = "SELECT * FROM USERS WHERE email = '$email' AND password = '$password'";
 	$result = $mydb->query($query);
 	if ($mydb->errno != 0)
 	{
@@ -63,7 +173,7 @@ function doCreate($email,$password,$logs)
 	}
 	$msg = "Connected to Database\nCreating User Account".PHP_EOL;
 	array_push($logs,$msg);
-	$query = "INSERT INTO `USERS` (`email`, `password`) VALUES ('$email','$password')";
+	$query = "INSERT INTO USERS (email, password) VALUES ('$email','$password')";
 	$result = $mydb->query($query);
 	if ($mydb->errno != 0)
 	{
@@ -105,6 +215,14 @@ function requestProcessor($request)
       $msg =  "Attempting Account Creation".PHP_EOL;
       array_push($logArray,$msg);
       return doCreate($request['email'],$request['password'],$logArray);
+    case "Recipe":
+      $msg = "Attempting to Add Recipe".PHP_EOL;
+      array_push($logArray,$msg);
+      return doAddRec($request['recipe'],$logArray);
+    case "Voting":
+      $msg = "Attempting to Adjust Vote".PHP_EOL;
+      array_push($logArray,$msg);
+      return doVoting($request['recipe'],$request['vote'],$logArray);
     case "validate_session":
       return doValidate($request['sessionId']);
   }
