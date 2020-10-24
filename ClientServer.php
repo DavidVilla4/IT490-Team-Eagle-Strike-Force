265bf1a6fbe7ee3123cbb85aed0fb9382ac1e250
allisonmcca <am2999@njit.edu>
2020-10-23 23:06:24 -0400

    connection to dmz

---
+++ ClientServer.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new rabbitMQClient("dataPull.ini","testServer");
+
+$request = array();
+$request['type'] = $argv[1];
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+
+
